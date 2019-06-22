4c1a191ce683275f92922ac99864876891862fe2
Cotos Catalin Claudiu <catalin_cotos07@yahoo.ro>
2019-06-22 18:40:20 +0300

    Add project files.

---
+++ Proiect biblioteca/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proiect_Biblioteca
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Meniu());
+            //Application.Run(new Form6());
+            //Application.Run(new Alege_carte());
+        }
+    }
+}
